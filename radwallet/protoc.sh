58643eed9a102fb81dc806839ea28ec6c7f22ee6
Gustavo Albino <gustavo.albino@ipnet.cloud>
2022-04-13 02:38:08 -0300

    refactoring

--- radwallet/protoc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-protoc --proto_path $GOOGLEAPISDIR  --proto_path .  --go_out=generated --go_opt=paths=source_relative --go-grpc_out=generated --go-grpc_opt=paths=source_relative profile.proto auth.proto
--- radwallet/reconfigure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-RANDOM=$$
-NAME=rad-config-$RANDOM
-
-protoc --proto_path $GOOGLEAPISDIR  --proto_path . --descriptor_set_out=api_descriptor.pb --include_imports --include_source_info auth.proto profile.proto
-gcloud api-gateway api-configs create $NAME --api=radwallet --project radwallet-dev --grpc-files=api_descriptor.pb,api_config.yaml
-gcloud api-gateway gateways update rad-gateway --location us-east1 --api=radwallet --api-config=$NAME
